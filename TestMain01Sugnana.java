2f546182dcd499968c73c240ab3c885c7ee6a69d
sugnana <sugnana@gmail.com>
2022-09-29 10:56:34 +0530

    SecondCommit29SEO2022

--- TestMain01Sugnana.java
+++ TestMain01Sugnana.java
@@ -1,5 +1,6 @@
 public TestMain{
 	public static void main(Strng args[]){
 		System.out.println("Welcome to JavaCode from Sugnana");
+		System.out.println("Welcome to JavaCode from Sugnana");
 	}
 }
