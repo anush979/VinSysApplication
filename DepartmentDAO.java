e1cc0292b2e74d69fb1d411255c744240d720ddb
sugnana <sugnana@gmail.com>
2022-09-29 11:04:00 +0530

    ThirdCommit29Sep2022

---
+++ DepartmentDAO.java
@@ -0,0 +1,6 @@
+public EmployeeDAO{
+	public static void main(Strng args[]){
+		System.out.println("Welcome to JavaCode from Sugnana");
+		System.out.println("Welcome to JavaCode from Sugnana");
+	}
+}
